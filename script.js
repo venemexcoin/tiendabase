5f605354bcfa927b6174d5fb9d246098431f850c
venemexcoin <venemexcoin@gmail.com>
2020-11-18 19:08:11 -0600

    commit Tienda en html,css y js

---
+++ script.js
@@ -0,0 +1,47 @@
+console.log('conectado')
+
+
+const MenuItems = document.getElementById("MenuItems");
+
+MenuItems.style.maxHeight = "0px";
+
+const menutoggle = () => {
+    if (MenuItems.style.maxHeight == "0px")
+    {
+        MenuItems.style.maxHeight = "200px"
+    }
+    else 
+    {
+        MenuItems.style.maxHeight = "0px"
+    } 
+
+}
+
+//-------- js for product gallery
+
+const ProductImg = document.getElementById("ProductImg")
+
+const SmallImg = document.getElementsByClassName("small-img")
+
+SmallImg[0].onclick = function() {
+    ProductImg.src = SmallImg[0].src;
+}
+
+SmallImg[1].onclick = function() {
+    ProductImg.src = SmallImg[1].src;
+}
+
+SmallImg[2].onclick = function() {
+    ProductImg.src = SmallImg[2].src;
+}
+
+SmallImg[3].onclick = function() {
+    ProductImg.src = SmallImg[3].src;
+}
+
+SmallImg[4].onclick = function() {
+    ProductImg.src = SmallImg[4].src;
+}
+
+
+
